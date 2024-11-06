6f2a6a4029e1eacdee8f11c17d35909d1d211d4f
Silence20240313 <liangxiaoli378@gmail.com>
2024-11-07 07:45:51 +0900

    springboot书城案例

---
+++ TestSpringBoot/src/main/java/com/msb/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.msb.controller;
+
+import com.msb.pojo.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.msb.service.BookService;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+    @Autowired
+    private BookService bookService;
+    @RequestMapping("/findBooks")
+    @ResponseBody
+    public String findBooks(){
+        List list = bookService.findAllBooks();
+        String s = "";
+        for (int i = 0; i < list.size(); i++) {
+             Book b = (Book)list.get(i);
+             s += b.getName();
+             s += b.getAuthor();
+        }
+        return s;
+    }
+}
